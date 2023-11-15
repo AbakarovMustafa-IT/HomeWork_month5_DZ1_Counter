cd2cd02826c021773fd98ab93a382fd7efc6ea3a
AbakarovMustafa-IT <webit2802@gmail.com>
2023-11-15 09:03:20 +0600

    HomeWork_month5_DZ1_Counter

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './pages/store';
+import Counter from './Counter';
+import './style.css'
+
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+};
+
+export default App;
